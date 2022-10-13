cc4cf08cb506aa0f814bb75c7bd03242ba978804
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2022-10-12 21:08:49 -0400

    Time: 21 ms (36.35%), Space: 54.9 MB (16.45%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,19 +1,18 @@
 class Solution {
-    public static String[] sortPeople(String[] names, int[] heights) {
-        Map<Integer, String> people = new HashMap<Integer, String>();
-        List<Integer> sortedHeights = new ArrayList<>();
-        String[] sortedNames = new String[names.length];
+    public String[] sortPeople(String[] names, int[] heights) {
+        // #names == #heights
+        int n = names.length;
 
-        for (int i=0; i<heights.length; i++)
-            people.put(heights[i], names[i]);
-
-        for (int h: heights) sortedHeights.add(h);
-        Collections.sort(sortedHeights);
-        Collections.reverse(sortedHeights);
-
-        for (int i=0; i<sortedHeights.size(); i++)
-            sortedNames[i] = people.get(sortedHeights.get(i));
+        // init Integer[] array 0...n indices
+        Integer[] idx = new Integer[n];
+        for(int i=0; i<n; i++) idx[i] = i;
         
-        return sortedNames;
+        // sort the indices w/ custom comparator
+        Arrays.sort(idx, (a,b)->(heights[b]-heights[a]));
+        
+        // String[] array of names
+        String[] ans = new String[n];
+        for(int i=0; i<n; i++) ans[i] = names[idx[i]];
+        return ans;
     }
 }
