29364def52a16827e40473efcd53d16caa171f7b
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2022-11-13 22:43:44 -0500

    Time: 34 ms (40.66%), Space: 13.3 MB (89.01%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -8,5 +8,4 @@ class Solution(object):
         for i in range(0, len(rings) - 1, 2): ringCount[int(rings[i+1])] += rings[i]
         sum = 0
         for r in ringCount: sum += 1 if "B" in r and "G" in r and "R" in r else 0
-        
         return sum
