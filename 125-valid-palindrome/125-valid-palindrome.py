30b66db22a03dacbd15ed5c89003ed026634b055
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2022-11-18 18:36:11 -0500

    Time: 311 ms (28.11%), Space: 14.5 MB (67.03%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        ans = ""
+        for letter in s:
+            ans += letter.lower() if letter.isalnum() else ""
+        
+        return ans == ans[::-1]
