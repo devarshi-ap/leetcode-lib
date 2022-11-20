cec402fc68a3e72de80cc86e0ec18e2ebe0b384b
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2022-11-19 19:47:34 -0500

    Time: 53 ms (56.39%), Space: 14.3 MB (42.05%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        arr = []
+        res = []
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
-                arr.append("FizzBuzz")
+                res.append("FizzBuzz")
             elif i % 3 == 0:
-                arr.append("Fizz")
+                res.append("Fizz")
             elif i % 5 == 0:
-                arr.append("Buzz")
+                res.append("Buzz")
             else:
-                arr.append(str(i))
-        return arr
+                res.append(str(i))
         
+        return res
