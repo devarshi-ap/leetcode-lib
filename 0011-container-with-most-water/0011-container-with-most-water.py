eee0b7ca3fef53258cf889cc8ed64e96095d5e95
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-15 19:28:06 -0400

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 99 ms (68.67%), Space: 28.5 MB (67.72%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,13 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left, right, maxArea = 0, len(height)-1, 0
+        # naive: nested for-loop calculate & compare maxArea O(n^2)
+        # 1. init pointers
+        left, right = 0, len(height)-1
+        maxArea = 0
+
+        # 2. setup algo invariant
         while left < right:
-            currArea = (right - left) * (min(height[right], height[left]))
+            # 3. do calculations
+            currArea = (right-left)*(min(height[left], height[right]))
             maxArea = max(maxArea, currArea)
-            if height[left] > height[right]:
-                # L-line taller than R-line, move R in
+            #. 4. move pointer(s) accordingly
+            if (height[left] > height[right]):
+                # L-height > R-height
                 right -= 1
             else:
-                # R-line taller (or equal to; same difference) than L-line, move L in
+                # L-height > R-height
+                # L-height = R-height
                 left += 1
+        
         return maxArea
