976e7a9172764a3c9789d6c0150cff84be175841
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-17 19:05:03 -0400

    0283-MOVE-ZEROES Stats: Time: 7 ms (45.05%), Space: 18.8 MB (79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        idx = 0 # points to first 0
+        idx = 0 # points to first zero
         for i in range(len(nums)):
+            # swap if 0-1
             if nums[i] != 0:
-                nums[i], nums[idx] = nums[idx], nums[i]
+                nums[idx], nums[i] = nums[i], nums[idx]
                 idx += 1
+        
