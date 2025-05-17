38f9404344fadbf2d9112968868b793a9a2803be
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-17 18:54:36 -0400

    0283-MOVE-ZEROES Stats: Time: 3 ms (81.26%), Space: 18.8 MB (79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,21 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums) > 1:
-            left, right = 0, 1
-            while right <= len(nums)-1:
-                if nums[left]==0 and nums[right]!=0: # ie. 0, 1
-                    nums[left], nums[right] = nums[right], nums[left] # swap
-                    left += 1
-                    right += 1
-                elif nums[left]!=0 and nums[right]==0: # ie. 1, 0
-                    left += 1
-                    right += 1
-                elif nums[left]!=0 and nums[right]!=0: # ie. 1, 1
-                    left += 1
-                    right += 1
-                else: # ie. 0, 0
-                    right += 1
-
-# [4,2,4,0,0,3,0,5,1,0]
-#  L R
+        idx = 0 # points to first 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[idx] = nums[idx], nums[i]
+                idx += 1
