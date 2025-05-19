978a0ccf4987dd5843f8140b8228fcfc48c82fa7
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-19 14:17:57 -0400

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 17.8 MB (53.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,7 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # naive: count # 0s/1s/2s in. hashtable, then overwrite while decrementing held-count
+        """
+        # naive: count # 0s/1s/2s in. hashtable, then overwrite while decrementing held-count (O(n))
+        # naive suffices here as it's just a simple in-place sorting question
+
         count = {0: 0, 1: 0, 2: 0}
         for x in nums:
             count[x] = 0 if x not in count else count[x] + 1
@@ -17,6 +20,20 @@ class Solution:
                 idx += 1
             else:
                 color += 1
+        """
 
-            
+        red, insptr, blue = 0, 0, len(nums)-1
+        while insptr <= blue: # while inspector is not done inspecting all nums
+            if nums[insptr] == 0: # inspector found 0
+                # [pass/swap to red] & [move red up (to an unknown)]
+                nums[red], nums[insptr] = nums[insptr], nums[red]
+                insptr += 1
+                red += 1
+            elif nums[insptr] == 1: # inspector found 1
+                # desired! inspector inspect next number
+                insptr += 1
+            else: # inspector found 2
+                # [pass/swap to blue] & [move blue down (to an unknown)]
+                nums[blue], nums[insptr] = nums[insptr], nums[blue]
+                blue -= 1
 
