f34d7b206729412a1756ab15ada6a63b6675a004
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-20 19:25:48 -0400

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 18 MB (32.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,22 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        targets = [] # holds closing brackets
-        pairs = {"(": ")", "[": "]", "{": "}"}
+        stack = [] # holds closing brackets
+        brackets = {"(": ")", "[": "]", "{": "}"}
 
-        for bracket in s:
-            if bracket in pairs.keys():
+        for b in s:
+            if b in brackets:
                 # ([{ --> add closing counterpart to targets[]
-                targets.append(pairs[bracket])
+                stack.append(brackets[b])
             else:
-                # read CLOSING )]}, make sure its opening counterpart is top of targets[] before popping
-                if targets:
-                    if targets[-1] == bracket:
-                        targets.pop()
-                    else:
-                        return False
+                # read CLOSING )]}, make sure its opening counterpart is top of stack before popping
+                if stack and stack[-1] == b:
+                        stack.pop()
                 else:
                     # list is empty, unbalanced brackets
                     return False
         
         # targets better be empty
-        return True if not targets else False
+        return not stack
