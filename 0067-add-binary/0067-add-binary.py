013a786918fff3b71912d43982c98729b139c9f9
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-22 00:34:38 -0400

    0067-ADD-BINARY Stats: Time: 3 ms (46.32%), Space: 17.7 MB (73.75%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,4 +1,38 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        intA, intB = int(a, 2), int(b, 2)
-        return str(bin(intA + intB))[2:]
+        """
+            C
+         10011
+             a
+            
+           111
+             b
+        -------
+             S
+        
+        S = a + b + C
+        """
+        aPtr, bPtr = len(a)-1, len(b)-1
+        result = ""
+        carry = 0
+
+        # keep running until both Ptr's reach 0 OR if carry = 1 (currSum still possible)
+        while aPtr >= 0 or bPtr >= 0 or carry:
+            # convert each digit (=0 if digit not indexable)
+            digitA = int(a[aPtr]) if aPtr >= 0 else 0
+            digitB = int(b[bPtr]) if bPtr >= 0 else 0
+            currSum = digitA + digitB + carry
+            
+            # SUM -> ACTION
+            # 0 -> all are 0 (result+="0", carry=0)
+            # 1 -> one is 1 (result+="1", carry=0)
+            # 2 -> two were 1 (result+="0", carry=1)
+            # 3 -> three were 1 (result+="1", carry=1)
+
+            result += str(currSum % 2)
+            carry = 1 if currSum >= 2 else 0
+
+            aPtr -= 1
+            bPtr -= 1
+
+        return result[::-1]
