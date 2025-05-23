49a8d66bf7d4049002b2edb564632c7248cbf5ec
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-22 20:23:45 -0400

    0940-FRUIT-INTO-BASKETS Stats: Time: 181 ms (65.37%), Space: 23.6 MB (78.12%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,28 +1,18 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        # note --> window expands by right++, shrinks by left++
-        # Tracks the count of each fruit in the current window
-        basket = {}
-        left = 0
-        maxFruits = 0
+        basket = {} # fruitType -> times Seen
+        maxL = 0
+        start = 0
 
-        # window = [left, right]
-        for right in range(len(fruits)):
-            # new fruit entry OR increment current fruit
-            fruit = fruits[right]
-            basket[fruit] = basket.get(fruit, 0) + 1
+        for end in range(len(fruits)):
+            fruit = fruits[end]
+            basket[fruit] = basket.get(fruit, 0) + 1 # new entry or increment
 
-            # If we have more than 2 types, shrink the window from the left
             while len(basket) > 2:
-                leftFruit = fruits[left]
-                basket[leftFruit] -= 1
-                if basket[leftFruit] == 0:
-                    del basket[leftFruit]
-                left += 1
-
-            # Update max fruits collected in any valid window
-            maxFruits = max(maxFruits, right - left + 1)
-
-        return maxFruits
+                basket[fruits[start]] -= 1
+                if basket[fruits[start]] == 0:
+                    del basket[fruits[start]]
+                start += 1
+            
+            maxL = max(maxL, end - start + 1)
+        return maxL
