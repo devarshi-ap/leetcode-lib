2d53366624b3230eb978c11bfb117bc492611511
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-29 20:56:33 -0400

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 37 ms (64.75%), Space: 17.9 MB (34.02%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -3,13 +3,12 @@ class Solution:
         result = ""
         i = 0
 
-        while i < len(word1) and i < len(word2): # 0 1
+        while i < len(word1) and i < len(word2): # exhaust i until shorter word exhausted
             result += word1[i]
             result += word2[i]
             i += 1
         
-        # apbq, now add rs from longer word2
-        result += word1[i:]
-        result += word2[i:]
+        # now add rest from both words (shorter word returns "")
+        result += word1[i:] + word2[i:]
 
         return result
