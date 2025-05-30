d5b22ec5f7741aea2e12fa59b6dfd06f7c956614
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-30 14:21:36 -0400

    0345-REVERSE-VOWELS-OF-A-STRING Stats: Time: 3 ms (99.77%), Space: 18.6 MB (69.75%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,24 +4,25 @@ class Solution:
         naive approach: 2-stride:
         1st stride (l-r): create stack of vowels
         2nd stride (l-r): swap out current vowel with vowels.pop()
-
         O(2N)
+        -----
+        vowels = [x for x in s if x.lower() in ['a', 'e', 'i', 'o', 'u']]
+        result = ""
+        for i in range(len(s)):
+            result += vowels.pop() if s[i].lower() in ['a', 'e', 'i', 'o', 'u'] else s[i]
+        return result
+        -----
 
-        Optimized --> Two Pointers:
+        Optimized --> Two Pointers (O(N)):
         icecream
         l      r
-        keep moving l in until it locks on a vowel -> then same for r
-        swap
-        increment both
+        
+        while l < r
+            move l until it locks on a vowel
+            move r until it locks on a vowel
+            swap
+            move both in
         """
-
-        # vowels = [x for x in s if x.lower() in ['a', 'e', 'i', 'o', 'u']]
-        # result = ""
-        
-        # for i in range(len(s)):
-        #     result += vowels.pop() if s[i].lower() in ['a', 'e', 'i', 'o', 'u'] else s[i]
-        
-        # return result
 
         l, r = 0, len(s) - 1
         s = list(s)
