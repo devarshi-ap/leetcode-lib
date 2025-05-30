b674a41afcbed7a01dcd9d585e761a8d6b1cf4ca
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2025-05-30 14:55:02 -0400

    1146-GREATEST-COMMON-DIVISOR-OF-STRINGS Stats: Time: 7 ms (9.12%), Space: 17.8 MB (45.42%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -2,11 +2,10 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         smallerN = min(len(str1), len(str2))
         gcdStr = ""
-        for i in range(1, smallerN+1):
+        i = 1
+        while i < smallerN+1:
             if str1[:i] == str2[:i]:
-                needed_for_One = len(str1) // i
-                needed_for_Two = len(str2) // i
-                print(str1[:i], needed_for_One, needed_for_Two)
-                if str1[:i]*needed_for_One == str1 and str1[:i]*needed_for_Two == str2:
+                if str1[:i]*(len(str1) // i) == str1 and str1[:i]*(len(str2) // i) == str2:
                     gcdStr = str1[:i]
+            i += 1
         return gcdStr
