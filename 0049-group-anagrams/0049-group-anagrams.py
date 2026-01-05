5c898a4a247f13a2de67a6ca6881a8d2d306fccc
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-01-05 17:17:08 -0500

    0049-GROUP-ANAGRAMS Stats: Time: 15 ms (50.28%), Space: 22.1 MB (27.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,14 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        bases = defaultdict(list)
-        for s in strs:
-            sortedStr = ''.join(sorted(s))
-            bases[sortedStr].append(s)
-        
-        return list(bases.values())
+        groups = defaultdict(list) # countMap -> anagrams[]
+        for x in strs:
+            countMap = [0] * 26 # a ... z
+            for c in x:
+                # ie. "abba" = [2a, 2b, 0c, ..., 0z]
+                countMap[ord(c) - ord("a")] += 1
+                
+            groups[tuple(countMap)].append(x) # {[2a, 2b, 0c, ..., 0z]: [abba, baba, ...]}
+        return list(groups.values())
+
