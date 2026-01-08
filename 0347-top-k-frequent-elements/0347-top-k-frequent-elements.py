3355f5e9e28532ec0a5e497bf0907ef6f570778c
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-01-08 00:44:36 -0500

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 13 ms (13.66%), Space: 24.8 MB (5.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,19 +7,17 @@ class Solution:
         for x in nums:
             freqMap[x] += 1
         
-        # bucketing (index = freq)
+        # Bucketing (index = freq)
         buckets = [[] for _ in range(len(nums) + 1)]
         
         for val,freq in freqMap.items():
             buckets[freq].append(val)
         
-        # get top k from buckets (count backwards until k-elements are collected)
+        # Count backwards in buckets until k-elements are collected
         results = []
         for i in range(len(buckets)-1, -1, -1):
             for x in buckets[i]:
-                if len(results) < k:
-                    results.append(x)
-                else:
-                    break
-        return results
+                results.append(x)
+                if len(results) == k:
+                    return results
 
