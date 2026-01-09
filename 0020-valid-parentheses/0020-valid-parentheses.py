be70544e47d1755e2d94525393175a67aae4492f
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-01-09 01:26:36 -0500

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 19.4 MB (12.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = [] # holds closing brackets
-        brackets = {"(": ")", "[": "]", "{": "}"}
-
-        for b in s:
-            if b in brackets:
-                # ([{ --> add closing counterpart to targets[]
-                stack.append(brackets[b])
-            else:
-                # )]} --> make sure its opening counterpart is top of stack before popping
-                if stack and stack[-1] == b:
-                        stack.pop()
-                else:
-                    # list is empty, unbalanced brackets
-                    return False
+        bracketStack = []
+        for x in s:
+            if x in "([{": # x is opening bracket add to stack
+                bracketStack.append(x)
+            else: # x is closing bracket (should pop its expected from stack)
+                match x:
+                    case ")":
+                        if (len(bracketStack) == 0) or bracketStack.pop() != "(":
+                            return False
+                    case "]":
+                        if (len(bracketStack) == 0) or bracketStack.pop() != "[":
+                            return False
+                    case "}":
+                        if (len(bracketStack) == 0) or bracketStack.pop() != "{":
+                            return False
         
-        # stack better be empty
-        return not stack
+        return True if len(bracketStack) == 0 else False
