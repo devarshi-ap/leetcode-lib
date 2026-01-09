ef35d099f708763b89de41af91ec5a2527930ba1
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-01-09 01:31:22 -0500

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 19.4 MB (12.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,10 +7,13 @@ class Solution:
         }
         bracketStack = []
         for x in s:
-            if x in "([{": # x is opening bracket --> add to stack
+            if x in "([{":
+                # x is opening bracket --> add to stack
                 bracketStack.append(x)
-            else: # x is closing bracket --> should pop its expected from stack
+            else:
+                # x is closing bracket --> should expect its own opening from stack pop
                 if (len(bracketStack) == 0) or bracketStack.pop() != closeToOpenMap.get(x):
-                            return False
+                    return False
         
+        # stack should be empty (closed all opening brackets in stack)
         return True if len(bracketStack) == 0 else False
