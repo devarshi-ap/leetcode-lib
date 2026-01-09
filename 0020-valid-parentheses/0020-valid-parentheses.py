a0c51cd3fd494ab3681b5d4361db359d00259ea8
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-01-09 01:29:36 -0500

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 19.4 MB (12.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        closeToOpenMap = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
         bracketStack = []
         for x in s:
-            if x in "([{": # x is opening bracket add to stack
+            if x in "([{": # x is opening bracket --> add to stack
                 bracketStack.append(x)
-            else: # x is closing bracket (should pop its expected from stack)
-                match x:
-                    case ")":
-                        if (len(bracketStack) == 0) or bracketStack.pop() != "(":
-                            return False
-                    case "]":
-                        if (len(bracketStack) == 0) or bracketStack.pop() != "[":
-                            return False
-                    case "}":
-                        if (len(bracketStack) == 0) or bracketStack.pop() != "{":
+            else: # x is closing bracket --> should pop its expected from stack
+                if (len(bracketStack) == 0) or bracketStack.pop() != closeToOpenMap.get(x):
                             return False
         
         return True if len(bracketStack) == 0 else False
