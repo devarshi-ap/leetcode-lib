f522cba7196adcf8dd12e2224c2c6a5b80036cc4
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-12 02:22:10 -0400

    0645-SET-MISMATCH Stats: Time: 14 ms (52.08%), Space: 20.5 MB (79.26%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -3,16 +3,13 @@ class Solution:
         index_targets = [0]*len(nums)
         dupe, lost = 0, 0
 
-        for x in nums:
-            idx = x-1
-            if index_targets[idx] == -1: # already marked
+        for i, x in enumerate(nums):
+            if index_targets[x-1] == -1: # already marked
                 dupe = x
             else:
-                index_targets[idx] = -1 # mark
+                index_targets[x-1] = -1 # mark
         
-        print(index_targets)
         for i, x in enumerate(index_targets):
-            print(x, i)
             if x == 0:
                 lost = i+1
         
