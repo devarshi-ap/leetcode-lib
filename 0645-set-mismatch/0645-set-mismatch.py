7c181412b48de41f84370ae383585e4bcf06f200
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-12 02:29:30 -0400

    0645-SET-MISMATCH Stats: Time: 5 ms (90.06%), Space: 21.1 MB (27.28%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,5 +1,7 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        # In-Place Negative Marking Approach
+        """
         index_targets = [0]*len(nums)
         dupe, lost = 0, 0
 
@@ -14,4 +16,17 @@ class Solution:
                 lost = i+1
         
         return [dupe, lost]
-            
+        """
+
+        # Math Sum_expected=(n*[n+1])/2
+        n = len(nums)
+        expectedSum = (n*(n+1)) // 2 # expected sum (formula)
+        actualSum = sum(nums) # actual sum
+        setSum = sum(set(nums)) # set of nums (without duplicate)
+
+        duplicate = actualSum - setSum
+        lost = expectedSum - setSum
+
+        return [duplicate, lost]
+        
+        
