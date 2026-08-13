045e135069db15420e67bd0eda958aa806024af9
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-13 00:17:29 -0400

    1482-HOW-MANY-NUMBERS-ARE-SMALLER-THAN-THE-CURRENT-NUMBER Stats: Time: 3 ms (79.94%), Space: 19.3 MB (21.96%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -2,6 +2,16 @@ class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         # naive = nested for loop (O(n^2))
         
-        # sorted array's index of X is how many value's are smaller than it
+        # "index of X in sorted array = how many value's are smaller than it
+        # ie. index of 8 in [1,2,2,3,8] is 4 (larger than 4 others)"
         
-        return [sorted(nums).index(x) for x in nums]
+        sorted_nums = sorted(nums) # [1,2,2,3,8]
+
+        # {1:0, 2:1, 3:3, 8:4}
+        lookup = {}
+        for i, num in enumerate(sorted_nums):
+            if num not in lookup:
+                lookup[num] = i
+
+        #[1,2,2,3,8] --> lookup --> [0,1,1,3,4]
+        return [lookup[x] for x in nums]
