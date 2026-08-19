1355ca0f2ab4024eab1205c50ff034cec1fbc12f
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-19 12:50:16 -0400

    0181-EMPLOYEES-EARNING-MORE-THAN-THEIR-MANAGERS Stats: Time: 450 ms (31.9%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-SELECT e1.name as Employee # only need to show employee name
-FROM Employee as e1
-JOIN Employee as e2
-ON e1.managerId = e2.id # e2 is manager
-WHERE e1.salary > e2.salary
+SELECT
+    e1.name as Employee # only need to show employee name
+FROM
+    Employee as e1 # e1=emp
+JOIN
+    Employee as e2 # e2=manager
+ON
+    e1.managerId = e2.id
+WHERE
+    e1.salary > e2.salary
