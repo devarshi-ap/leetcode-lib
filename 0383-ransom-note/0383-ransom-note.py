0e8b2ad8b709ec994a6c75aa2839e6cca9cfe5d5
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-23 14:56:19 -0400

    0383-RANSOM-NOTE Stats: Time: 27 ms (34.44%), Space: 19.7 MB (8.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,17 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        magazineDict = {}
-        for chr in magazine:
-            magazineDict[chr] = magazineDict.get(chr, 0) + 1
-
-        for chr in ransomNote:
-            print(chr, magazineDict)
-            if chr in magazineDict:
-                if magazineDict[chr] == 1:
-                    del magazineDict[chr]
-                else:
-                    magazineDict[chr] -= 1
-            else:
-                return False
+        # create char-freq for magazine, then reduce from magazine while iterating through ransomNote
+        char_freq = {}
+        for c in magazine:
+            char_freq[c] = char_freq.get(c, 0) + 1 # gets c's value (0 if not in dict) then + 1
         
+        for c in ransomNote:
+            if c not in char_freq:
+                return False
+            elif char_freq[c] >= 2:
+                char_freq[c] -= 1
+            else:
+                char_freq.pop(c)
         return True
