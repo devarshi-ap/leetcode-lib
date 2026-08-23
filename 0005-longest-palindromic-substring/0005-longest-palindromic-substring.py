024c5d0cf6ec4598314889b2ed2885d4a10becbf
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-23 00:31:18 -0400

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 722 ms (30.08%), Space: 19.2 MB (69.39%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,14 +4,14 @@ class Solution:
         maxPal = ""
 
         for i in range(len(s)):
-            # odd length expansion (l,r move outwards same pace)
+            # odd length expansion (l,r move outwards same time; x, _x_, __x__)
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]: # inbounds + chars match
                 maxPal = max(maxPal, s[l:r+1], key=len)
                 l -= 1
                 r += 1
             
-            # same, but now repeat for even length expansion (r=i+1 to begin)
+            # same, but now repeat for even length expansion (r=i+1 to begin; xr, _xr_, __xr__)
             l, r = i, i+1
             while l >= 0 and r < len(s) and s[l] == s[r]: # inbounds + chars match
                 maxPal = max(maxPal, s[l:r+1], key=len)
