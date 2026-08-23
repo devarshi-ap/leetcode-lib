628937a1262dfb8e9271a979fdb84e3abf82cdc9
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-23 15:01:03 -0400

    0383-RANSOM-NOTE Stats: Time: 16 ms (67.37%), Space: 19.6 MB (72.7%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,5 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # False if ransomNote uses more chars than available
+        if len(ransomNote) > len(magazine):
+            return False
+
         # create char-freq for magazine, then reduce from magazine while iterating through ransomNote
         char_freq = {}
         for c in magazine:
