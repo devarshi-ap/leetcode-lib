91bf5dfbddb81268f407d01eebdc774adf8ef5df
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-27 18:57:14 -0400

    0015-3SUM Stats: Time: 635 ms (58.27%), Space: 23 MB (10.98%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,30 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        # sort arr, then run TwoPointers within for-loop
         nums.sort()
-        result = []
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        combos = []
+        print(nums)
+
+        for i in range(len(nums)-2): # - 2 saves 2 spaces right of i for L,R
+            if i > 0 and nums[i] == nums[i-1]:  # skip duplicate i
                 continue
-            left = i + 1
-            right = len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
-                else:
-                    result.append([nums[i], nums[left], nums[right]])
+            # now just 2SUM!
+            l, r = i+1, len(nums)-1
+            print(i, l, r)
+            while l < r:
+                c_sum = nums[i] + nums[l] + nums[r]
+                if c_sum > 0: # too much (<--R)
+                    r -= 1
+                elif c_sum < 0: # too little (L-->)
+                    l += 1
+                else: # perfecto (save combo, move both inward)
+                    combos.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:  # skip duplicate l
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:  # skip duplicate r
+                        r -= 1
 
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-        return result
-
-            
+        return combos
