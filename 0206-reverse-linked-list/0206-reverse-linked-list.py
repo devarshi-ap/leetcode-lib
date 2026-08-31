089baedba284e1783efdbfd5746fb5641672cf40
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-08-31 15:56:10 -0400

    0206-REVERSE-LINKED-LIST Stats: Time: 0 ms (100%), Space: 20.5 MB (28.11%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,17 +5,28 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # pointer walk + stack (FILO)
+        """ Naive Sol'n = pointer walk + stack (FILO)
         stack = []
         ansHead = dummy = ListNode(0)
-
         while head:
             stack.append(head.val)
             head = head.next
             # print(stack)
-        
         while len(stack) > 0:
             dummy.next = ListNode(stack.pop())
             dummy = dummy.next
-        
         return ansHead.next
+        """
+
+        # Optimized Sol'n = 3-ptr walk (prev/curr/next) and flip as you go
+        prev = None
+        curr = head
+        
+        while curr:
+            nxt = curr.next # save next node
+            curr.next = prev # flip link
+            # shift ptr's
+            prev = curr
+            curr = nxt
+        
+        return prev
