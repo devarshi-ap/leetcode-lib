ef2f4ae6aa3f8b0bbd56be593ce633b071d988f0
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2026-09-21 16:56:55 -0400

    0138-COPY-LIST-WITH-RANDOM-POINTER Stats: Time: 43 ms (83.04%), Space: 20.2 MB (19.73%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -19,16 +19,9 @@ class Solution:
             curr = curr.next
 
         # second pass: assigning .next/.random to newNodes
-        # for a in nodeMap:
-        #     A = nodeMap[a]
-        #     A.next = nodeMap[a.next] if a.next else None
-        #     A.random = nodeMap[a.random] if a.random else None
-
-        a = head
-        while a:
-            A = nodeMap[a]
-            A.next = nodeMap[a.next] if a.next else None
-            A.random = nodeMap[a.random] if a.random else None
-            a = a.next
+        for oldNode in nodeMap:
+            newNode = nodeMap[oldNode]
+            newNode.next = nodeMap[oldNode.next] if oldNode.next else None
+            newNode.random = nodeMap[oldNode.random] if oldNode.random else None
 
         return nodeMap[head]
